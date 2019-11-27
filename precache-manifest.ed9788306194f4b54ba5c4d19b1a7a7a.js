b903cc4d6c7343317e27dd660afb55e5d85f2b4b
Duke Capitan <dukecapitan@gmail.com>
2019-11-28 00:54:37 +0100

    Updates

---
+++ precache-manifest.ed9788306194f4b54ba5c4d19b1a7a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3192ef7599a49bb1a329a8899796483c",
+    "url": "/recipesreact/index.html"
+  },
+  {
+    "revision": "0f114f836a08c98146cf",
+    "url": "/recipesreact/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "fef9c53bc67b2790029c",
+    "url": "/recipesreact/static/css/main.17004fa1.chunk.css"
+  },
+  {
+    "revision": "0f114f836a08c98146cf",
+    "url": "/recipesreact/static/js/2.b6577a0f.chunk.js"
+  },
+  {
+    "revision": "fef9c53bc67b2790029c",
+    "url": "/recipesreact/static/js/main.877baa6e.chunk.js"
+  },
+  {
+    "revision": "32cb0aadc322f93a9928",
+    "url": "/recipesreact/static/js/runtime-main.0dc9b005.js"
+  }
+]);
